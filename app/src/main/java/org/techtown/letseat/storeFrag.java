3cda405851de6f3cded3a5e0d8a9851033cd1ec7
sukhae <sukhae@gmail.com>
2021-09-29 00:29:24 +0900

    리사이클러뷰 가게관리 추가

--- app/src/main/java/org/techtown/letseat/storeFrag.java
+++ app/src/main/java/org/techtown/letseat/storeFrag.java
@@ -4,23 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+
 import org.techtown.letseat.menu.Menu_Management;
 
 public class storeFrag extends Fragment {
 
+    private store_recyclerAdapter adapter = new store_recyclerAdapter();
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_store,container,false);
 
-        Button store_register_button = view.findViewById(R.id.store_register_button);
+        RecyclerView recyclerView = view.findViewById(R.id.store_recyclerView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setItems(new storeData().getItems());
+
+        ExtendedFloatingActionButton store_register_button = view.findViewById(R.id.store_register_button);
         store_register_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,14 +42,14 @@ public class storeFrag extends Fragment {
             }
         });
 
-        Button menu_management_button = view.findViewById(R.id.menu_management_button);
+        /*Button menu_management_button = view.findViewById(R.id.menu_management_button);
         menu_management_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), Menu_Management.class);
                 startActivity(intent);
             }
-        });
+        }); */
         return view;
     }
 }
---
+++ app/src/main/java/org/techtown/letseat/store_recyclerAdapter.java
@@ -0,0 +1,79 @@
+package org.techtown.letseat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+
+public class store_recyclerAdapter extends RecyclerView.Adapter<store_recyclerAdapter.ViewHolder>{
+
+    private ArrayList<storeItem> items = new ArrayList<>();
+    private Context context;
+
+    @NonNull
+    @Override
+    public store_recyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.store_recycle, parent, false);
+        ViewHolder viewHolder = new ViewHolder(itemView);
+        context = parent.getContext();
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull store_recyclerAdapter.ViewHolder viewHolder, int position) {
+
+        storeItem item = items.get(position);
+
+        viewHolder.storeIv.setImageResource(item.getSrc());
+        viewHolder.titleTv.setText(item.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void setItems(ArrayList<storeItem> items) {
+        this.items = items;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView storeIv;
+        TextView titleTv;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            storeIv = itemView.findViewById(R.id.storeIv);
+            titleTv = itemView.findViewById(R.id.titleTv);
+
+            itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION)
+                    {
+
+                    }
+                }
+            });
+
+        }
+    }
+}
