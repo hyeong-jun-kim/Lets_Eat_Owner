b9de18c29e8ca75e736bf202155db79a2682c61a
sukhae <sukhae@gmail.com>
2021-09-30 00:59:56 +0900

    주문 관리 리사이클러뷰 디자인 수정

--- app/src/main/java/org/techtown/letseat/order/OrderAdapter.java
+++ app/src/main/java/org/techtown/letseat/order/OrderAdapter.java
@@ -3,6 +3,7 @@ package org.techtown.letseat.order;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,38 +12,41 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import org.techtown.letseat.R;
+import org.techtown.letseat.storeItem;
+import org.techtown.letseat.store_recyclerAdapter;
 
 import java.util.ArrayList;
 
 public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder>{
     private ArrayList<Orderdata> Dataset;
+    private Context context;
     private Intent intent;
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        public TextView ordertime, ordertimetext ,tablenumber, tablenumbertext, menu, menulist, quantity, quantitytext, quantitymore, moremenu, moremenutext, request, requesttext, price, pricetext;
+        public TextView priceTv,requestTv, menuTv, tablenameTv, dateTv;
         public View view1;
-        public Button check_order, check_serving;
+        public Button okBtn, cancelBtn;
 
         public ViewHolder(View view){
             super(view);
-            ordertime = (TextView) view.findViewById(R.id.ordertime);
-            ordertimetext = (TextView) view.findViewById(R.id.ordertimetext);
-            tablenumber = (TextView) view.findViewById(R.id.tablenumber);
-            tablenumbertext = (TextView) view.findViewById(R.id.tablenumbertext);
-            menu = (TextView) view.findViewById(R.id.menu);
-            menulist = (TextView) view.findViewById(R.id.menulist);
-            quantity = (TextView) view.findViewById(R.id.quantity);
-            quantitytext = (TextView) view.findViewById(R.id.quantitytext);
-            quantitymore = (TextView) view.findViewById(R.id.quantitymore);
-            moremenu = (TextView) view.findViewById(R.id.moremenu);
-            moremenutext = (TextView) view.findViewById(R.id.moremenutext);
-            request = (TextView) view.findViewById(R.id.request);
-            requesttext = (TextView) view.findViewById(R.id.requesttext);
-            price = (TextView) view.findViewById(R.id.price);
-            pricetext = (TextView) view.findViewById(R.id.pricetext);
-            view1 = (View) view.findViewById(R.id.view1);
-            check_order = view.findViewById(R.id.check_order);;
-            check_serving = view.findViewById(R.id.check_serving);
+            priceTv = (TextView) view.findViewById(R.id.priceTv);
+            requestTv = (TextView) view.findViewById(R.id.requestTv);
+            menuTv = (TextView) view.findViewById(R.id.menuTv);
+            dateTv = (TextView) view.findViewById(R.id.dateTv);
+            tablenameTv = (TextView) view.findViewById(R.id.tablenameTv);
+
+            view.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION)
+                    {
+                        mListener.onItemClick(v, pos);
+                    }
+                }
+            });
         }
     }
 
@@ -54,11 +58,22 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder>{
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.order_recycle, parent, false);
-        return new ViewHolder(view);
+        OrderAdapter.ViewHolder viewHolder = new OrderAdapter.ViewHolder(view);
+        context = parent.getContext();
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull OrderAdapter.ViewHolder holder, int position){
+
+        Orderdata item = Dataset.get(position);
+
+        holder.priceTv.setText(item.getPriceTv());
+        holder.requestTv.setText(item.getRequestTv());
+        holder.menuTv.setText(item.getMenuTv());
+        holder.tablenameTv.setText(item.getTablenameTv());
+        holder.dateTv.setText(item.getDateTv());
+
         holder.itemView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -68,9 +83,23 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder>{
             }
         });
     }
+    public interface OnItemClickListener
+    {
+        void onItemClick(View v, int pos);
+    }
 
+    private OrderAdapter.OnItemClickListener mListener = null;
+
+    public void setOnItemClickListener(OrderAdapter.OnItemClickListener listener)
+    {
+        this.mListener = listener;
+    }
     @Override
     public int getItemCount(){
         return Dataset.size();
+    }
+
+    public void setItems(ArrayList<Orderdata> items) {
+        this.Dataset = items;
     }
 }
