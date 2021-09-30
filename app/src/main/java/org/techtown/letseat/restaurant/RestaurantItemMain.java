37b427f3c026ecbfdf655c2478af99b5df9a8b1b
KSB3870 <73988645+KSB3870@users.noreply.github.com>
2021-10-01 01:51:35 +0900

    09.30 Bug FIX 식당정보 상단탭 적용, 식당정보 아이템 ver 1.0

---
+++ app/src/main/java/org/techtown/letseat/restaurant/RestaurantItemMain.java
@@ -0,0 +1,56 @@
+package org.techtown.letseat.restaurant;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import org.techtown.letseat.R;
+import org.techtown.letseat.util.ViewPagerAdapter;
+
+
+public class RestaurantItemMain extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private RestaurantItemInfoFragment restaurant_Item_Info_Fragment;
+    private RestaurantItemMenuFragment restaurant_Item_Menu_fragment;
+    private RestaurantItemReviewFragment restaurant_Item_review_fragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurant_item);
+
+        viewPager = findViewById(R.id.view_pager);
+        viewPager.setOffscreenPageLimit(3);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        restaurant_Item_Info_Fragment = new RestaurantItemInfoFragment();
+        restaurant_Item_Menu_fragment = new RestaurantItemMenuFragment();
+        restaurant_Item_review_fragment = new RestaurantItemReviewFragment();
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
+        viewPagerAdapter.addFragment(restaurant_Item_Info_Fragment,"가게 정보");
+        viewPagerAdapter.addFragment(restaurant_Item_Menu_fragment,"메뉴 설정");
+        viewPagerAdapter.addFragment(restaurant_Item_review_fragment,"리뷰 관리");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager){
+
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                super.onTabReselected(tab);
+            }
+        });
+
+    }
+}
