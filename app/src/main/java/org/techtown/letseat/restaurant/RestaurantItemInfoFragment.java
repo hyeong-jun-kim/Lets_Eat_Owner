75321c731fac1130acc81a4fca922d8ac6b1cf27
taehyun24 <ahnth0316@naver.com>
2021-10-03 16:24:43 +0900

    10.03 work

--- app/src/main/java/org/techtown/letseat/restaurant/RestaurantItemInfoFragment.java
+++ app/src/main/java/org/techtown/letseat/restaurant/RestaurantItemInfoFragment.java
@@ -1,17 +1,42 @@
 package org.techtown.letseat.restaurant;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.techtown.letseat.R;
+import org.techtown.letseat.util.PhotoSave;
 
 public class RestaurantItemInfoFragment extends Fragment{
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private final int GET_GALLERY_IMAGE = 200;
+    private RadioButton singleMealYes, singleMealNo;
+    private AppCompatButton info_edit_btn;
+    private int aloneAble;
+    private TextView textView;
+    private final String[] items = {"한식", "중식", "일식", "양식"};
+    private String resType,image;
+    private ImageView restaurant_image = null;
+    private Bitmap bitmap;
 
     public RestaurantItemInfoFragment() {
         // Required empty public constructor
@@ -36,7 +61,82 @@ public class RestaurantItemInfoFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.restaurant_item_info_fragment, container, false);
+        View view = inflater.inflate(R.layout.restaurant_item_info_fragment, container, false);
+
+        singleMealYes = view.findViewById(R.id.store_register_yes);
+        singleMealNo = view.findViewById(R.id.store_register_no);
+        textView = view.findViewById(R.id.textView);
+
+        info_edit_btn = view.findViewById(R.id.info_edit_btn);
+        info_edit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (singleMealYes.isChecked()) {
+                    aloneAble = 1;
+                } else if (singleMealNo.isChecked()) {
+                    aloneAble = 0;
+                }
+            }
+        });
+
+        Spinner spinner = view.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_spinner_dropdown_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText(items[position]);
+                switch (position) {
+                    case 0:
+                        resType = "koreanFood";
+                        break;
+                    case 1:
+                        resType = "chineseFood";
+                        break;
+                    case 2:
+                        resType = "japaneseFood";
+                        break;
+                    case 3:
+                        resType = "westernFood";
+                        break;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                textView.setText("");
+            }
+        });
+
+        restaurant_image = (ImageView) view.findViewById(R.id.restaurant_image);
+        restaurant_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.
+                        setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
+        return view;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+
+            Uri selectedImageUri = data.getData();
+            restaurant_image.setImageURI(selectedImageUri);
+            BitmapDrawable drawable = (BitmapDrawable) restaurant_image.getDrawable();
+            bitmap = drawable.getBitmap();
+            bitmap = PhotoSave.resize(bitmap, getResources());
+            image = PhotoSave.BitmapToString(bitmap);
+        }
     }
 }
 
