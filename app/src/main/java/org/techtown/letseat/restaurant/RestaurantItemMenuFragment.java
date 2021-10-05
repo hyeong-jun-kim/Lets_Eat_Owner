15fe4ab4777cd14ad389428197e73e0672206fc3
sukhae <sukhae@gmail.com>
2021-10-05 22:49:05 +0900

    메뉴

--- app/src/main/java/org/techtown/letseat/restaurant/RestaurantItemMenuFragment.java
+++ app/src/main/java/org/techtown/letseat/restaurant/RestaurantItemMenuFragment.java
@@ -7,42 +7,36 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 import org.techtown.letseat.R;
+import org.techtown.letseat.menu.Menu;
+import org.techtown.letseat.menu.MenuAdapter;
 import org.techtown.letseat.menu.Menu_add;
 
 public class RestaurantItemMenuFragment extends Fragment {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+
     ExtendedFloatingActionButton menu_register_button;
-
-    public RestaurantItemMenuFragment() {
-        // Required empty public constructor
-    }
-
-    public static RestaurantItemMenuFragment newInstance(String param1, String param2) {
-        RestaurantItemMenuFragment fragment = new RestaurantItemMenuFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.restaurant_item_menu_fragment, container, false);
 
+        RecyclerView recyclerView = view.findViewById(R.id.rest_item_menu_recycler);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext(),LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        MenuAdapter adapter = new MenuAdapter();
+
+        adapter.addItem(new Menu("마라탕", "3000원", R.drawable.menuimg1));
+        adapter.addItem(new Menu("해물탕", "5000원", R.drawable.menuimg2));
+
+        recyclerView.setAdapter(adapter);
         menu_register_button = view.findViewById(R.id.menu_register_button);
         menu_register_button.setOnClickListener(new View.OnClickListener() {
             @Override
