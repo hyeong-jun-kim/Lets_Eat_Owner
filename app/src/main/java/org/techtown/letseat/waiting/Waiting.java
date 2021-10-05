7dc54acc63d1250b821a647e317d5df4e06d16ae
sukhae <sukhae@gmail.com>
2021-10-06 00:40:11 +0900

    대기자 관리 디자인

--- app/src/main/java/org/techtown/letseat/waiting/Waiting.java
+++ app/src/main/java/org/techtown/letseat/waiting/Waiting.java
@@ -1,32 +1,71 @@
 package org.techtown.letseat.waiting;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import org.techtown.letseat.R;
+
 public class Waiting {
-    String waiting_number;
-    String name;
-    String phonenum;
-    String person_number;
-    String reception_time;
+    String date,waitingnum, id, people, phonenumber;
+    int src;
 
+    public Waiting(String date, String waitingnum, String id, String people, String phonenumber, int src) {
+        this.date = date;
+        this.waitingnum = waitingnum;
+        this.id = id;
+        this.people = people;
+        this.phonenumber = phonenumber;
+        this.src = src;
+    }
 
-    public Waiting(String waiting_number, String name, String phonenum, String person_number,
-                   String reception_time){
-        this.waiting_number = waiting_number;
-        this.name = name;
-        this.phonenum = phonenum;
-        this.person_number = person_number;
-        this.reception_time = reception_time;
+    public String getDate() {
+        return date;
     }
-    public String getWaiting_number(){return  waiting_number;}
-    public String getName(){
-        return name;
+
+    public void setDate(String date) {
+        this.date = date;
     }
-    public String getPhonenum(){
-        return phonenum;
+
+    public String getWaitingnum() {
+        return waitingnum;
     }
-    public String getPerson_number(){
-        return person_number;
+
+    public void setWaitingnum(String waitingnum) {
+        this.waitingnum = waitingnum;
     }
-    public String getReception_time(){
-        return reception_time;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPeople() {
+        return people;
+    }
+
+    public void setPeople(String people) {
+        this.people = people;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public int getSrc() {
+        return src;
+    }
+
+    public void setSrc(int src) {
+        this.src = src;
     }
 }
